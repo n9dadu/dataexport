e8734cffc84776533324d3583087637cd7318eb6
n9dadu <n9dadu@gmail.com>
2026-09-21 11:38:44 +0200

    Create export-data.ps1

---
+++ export-data.ps1
@@ -0,0 +1,22 @@
+$SERVER = "localhost"
+$DATABASE = "dbname"
+$SQLFile = "sqlfile.sql"
+
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$ConnectionString = "Server=$SERVER;Database=$DATABASE;Integrated Security=True;"
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection $ConnectionString
+Write-Host "Open connection to database: $DATABASE on $SERVER"
+$SqlConnection.Open()
+
+Write-Host "Executing SQL script: $SQLFile on database: $DATABASE"
+$SqlCommand = $SqlConnection.CreateCommand()
+$SqlCommand.CommandText = Get-Content "$ScriptDir\$SQLFile" -Raw
+$SqlDataAdapter = New-Object System.Data.SqlClient.SqlDataAdapter $SqlCommand
+$DataSet = New-Object System.Data.DataSet
+$SqlDataAdapter.Fill($DataSet) | Out-Null
+$SqlConnection.Close()
+$table = $DataSet.Tables[0]
+Write-Host "SQL script execution completed. Exporting $($table.Rows.Count) rows to CSV."
+
+$table | Export-Csv -Path "$ScriptDir\Check-EmptyYEars.csv" -NoTypeInformation
+Write-Host "DONE. Results exported to CSV: $ScriptDir\Check-EmptyYEars.csv"
